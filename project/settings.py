739e5395805e4ddf2bfb03224143b85d938564ba
NicatDev <nicat254memmedov@gmail.com>
2024-03-26 01:06:27 +0400

    22

--- project/settings.py
+++ project/settings.py
@@ -40,7 +40,8 @@ CSRF_TRUSTED_ORIGINS = [
 
 CELERY_BROKER_URL = 'amqp://guest:guest@test.victoriassirri.az:5672/'
 CELERY_RESULT_BACKEND = 'django-db'
-
+CELERY_TASK_TRACK_STARTED = True
+CELERY_TASK_TIME_LIMIT = 30 * 60
 INSTALLED_APPS = [
     'modeltranslation',
     'django.contrib.admin',
